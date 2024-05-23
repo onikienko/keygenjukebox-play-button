fb197978910c4ee5bb3a25cb62a64a135af8dbb8
Mykhailo Onikiienko <michael.onikienko@gmail.com>
2024-05-22 18:22:32 -0700

    fix: correct badge update

--- mv3/src/background/background.js
+++ mv3/src/background/background.js
@@ -4,9 +4,16 @@ import {humanTime} from '../utils/utils';
 import {fetchAndUpdateList, getPlaylist} from './playlist';
 
 
-const updateTotalPlayedAndBadge = async (newTime = 0) => {
+const updateTotalPlayed = async (newTime = 0) => {
+    if (!newTime) return;
     const storage = await chrome.storage.local.get({[STORAGE_NAME.TOTAL_PLAYED]: 0});
     const totalPlayedMsec = storage[STORAGE_NAME.TOTAL_PLAYED] + newTime;
+    await chrome.storage.local.set({[STORAGE_NAME.TOTAL_PLAYED]: totalPlayedMsec});
+};
+
+const updateTotalPlayedBadge = async () => {
+    const storage = await chrome.storage.local.get({[STORAGE_NAME.TOTAL_PLAYED]: 0});
+    const totalPlayedMsec = storage[STORAGE_NAME.TOTAL_PLAYED];
     if (!totalPlayedMsec) {
         chrome.i18n.getMessage('p_defaultTitle');
         return;
@@ -17,7 +24,6 @@ const updateTotalPlayedAndBadge = async (newTime = 0) => {
     title += totalPlayedHuman.h ? ' ' + totalPlayedHuman.h + chrome.i18n.getMessage('p_hour') : ' 0' + chrome.i18n.getMessage('p_hour');
     title += totalPlayedHuman.m ? ' ' + totalPlayedHuman.m + chrome.i18n.getMessage('p_min') : ' 0' + chrome.i18n.getMessage('p_min');
     await chrome.action.setTitle({title});
-    await chrome.storage.local.set({[STORAGE_NAME.TOTAL_PLAYED]: totalPlayedMsec});
 };
 
 enableAlarm(ALARM_UPD_PLAYLIST, {
@@ -51,16 +57,30 @@ chrome.runtime.onMessage.addListener(async (message) => {
             await chrome.action.setTitle({
                 title: chrome.i18n.getMessage('p_nowPlaying') + '\n' + message.data.info,
             });
+            await updateTotalPlayed(message.data.playedMsec);
             break;
 
         case MSG_CMD.STOP:
             await chrome.action.setBadgeText({text: ''});
-            // await chrome.action.setTitle({title: ''});
+            await updateTotalPlayed(message.data.playedMsec);
+            await updateTotalPlayedBadge(message.data.playedMsec);
             break;
-    }
-    if (message.data?.playedMsec) {
-        updateTotalPlayedAndBadge(message.data.playedMsec);
     }
 });
 
-updateTotalPlayedAndBadge();
+self.addEventListener('install', (e) => {
+    console.log('sw install', e);
+});
+
+self.addEventListener('activate', (e) => {
+    console.log('sw activate', e);
+});
+// to avoid updating badge every time when sw wake up (possible during playing)
+const updateBadgeOnStartup = async () => {
+    const storage = await chrome.storage.session.get('flag');
+    if (storage['flag']) return;
+    await updateTotalPlayedBadge();
+    await chrome.storage.session.set({flag: 1});
+};
+
+updateBadgeOnStartup();
