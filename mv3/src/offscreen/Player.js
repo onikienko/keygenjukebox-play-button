97e157323e0b630cf4923aff479da736ed6d37db
Mykhailo Onikiienko <michael.onikienko@gmail.com>
2024-05-29 21:12:34 -0700

    style: refactor offscreen and Player

--- mv3/src/offscreen/Player.js
+++ mv3/src/offscreen/Player.js
@@ -1,27 +1,26 @@
-import logo128 from 'data-url:../images/128.png';
-import {KGM_MIRRORS} from '../consts';
 import {shuffleArray} from '../utils/utils';
 
 
 export class Player extends EventTarget {
     isPlaying = false;
-    #audio;
-    #playlist;
-    #curPlaylistPos;
+    #audio = new Audio();
+    #playlist = [];
+    #mirrors = [];
+    #curPlaylistPos = 0;
     #startTime = 0;
-    #curBasePathIndex = 0;
+    #curMirrorIndex = 0;
 
-    constructor(playlist) {
+    constructor(playlist, mirrors) {
         super();
+        this.#mirrors = mirrors;
         this.#playlist = playlist;
         this.#initPlaylist();
-        this.#audio = new Audio();
         this.#listenAudioEvents();
     }
 
     async play() {
         const playlistElement = this.#playlist[this.#curPlaylistPos];
-        this.#audio.src = KGM_MIRRORS[this.#curBasePathIndex] + playlistElement.p;
+        this.#audio.src = this.#mirrors[this.#curMirrorIndex] + playlistElement.p;
         try {
             await this.#audio.play();
             this.isPlaying = true;
@@ -33,7 +32,6 @@ export class Player extends EventTarget {
                 },
             }));
             this.#startTime = now;
-            this.#setMetadata(playlistElement.st);
         } catch (e) {
             console.log('Error playing', e);
         }
@@ -78,11 +76,11 @@ export class Player extends EventTarget {
         });
         this.#audio.addEventListener('error', () => {
             // try to play from another mirror
-            if (this.#curBasePathIndex >= KGM_MIRRORS.length - 1) {
+            if (this.#curMirrorIndex >= this.#mirrors.length - 1) {
                 this.#startTime = 0;
                 this.stop();
             } else {
-                this.#curBasePathIndex++;
+                this.#curMirrorIndex++;
                 this.play();
             }
         });
@@ -90,18 +88,6 @@ export class Player extends EventTarget {
             navigator.mediaSession.setActionHandler('nexttrack', () => this.next());
             navigator.mediaSession.setActionHandler('previoustrack', () => this.prev());
             navigator.mediaSession.setActionHandler('stop', () => this.stop());
-        }
-    }
-
-    #setMetadata(info) {
-        if ('mediaSession' in navigator) {
-            navigator.mediaSession.metadata = new MediaMetadata({
-                title: info,
-                artwork: [
-                    // add also 96, 192, 256, 384 and 512
-                    {src: logo128, sizes: '128x128', type: 'image/png'},
-                ],
-            });
         }
     }
 }
--- mv3/src/offscreen/offscreen.js
+++ mv3/src/offscreen/offscreen.js
@@ -1,4 +1,5 @@
-import {MSG_CMD} from '../consts';
+import logo128 from 'data-url:../images/128.png';
+import {KGM_MIRRORS, MSG_CMD} from '../consts';
 import {Player} from './Player';
 
 
@@ -10,8 +11,17 @@ let player;
 chrome.runtime.onMessage.addListener((message) => {
     if ('play' in message) {
         if (!player) {
-            player = new Player(message.play.playlist);
+            player = new Player(message.play.playlist, KGM_MIRRORS);
             player.addEventListener('play', (e) => {
+                if ('mediaSession' in navigator) {
+                    navigator.mediaSession.metadata = new MediaMetadata({
+                        title: e.detail.info,
+                        artwork: [
+                            // add also 96, 192, 256, 384 and 512
+                            {src: logo128, sizes: '128x128', type: 'image/png'},
+                        ],
+                    });
+                }
                 chrome.runtime.sendMessage({
                     cmd: MSG_CMD.PLAY,
                     data: {info: e.detail.info, playedMsec: e.detail.playedMsec},
