1fa836aef6b7747f4263350b24e7c036f6c24dd0
Mykhailo Onikiienko <michael.onikienko@gmail.com>
2024-07-04 17:57:58 -0700

    refactor: move Player initialization to function

--- mv3/src/offscreen/offscreen.js
+++ mv3/src/offscreen/offscreen.js
@@ -8,30 +8,37 @@ import {Player} from './Player';
  */
 let player;
 
+const initPlayer = (playlist, mirrors) => {
+    const pl = new Player(playlist, mirrors);
+    pl.addEventListener('play', (e) => {
+        if ('mediaSession' in navigator) {
+            navigator.mediaSession.metadata = new MediaMetadata({
+                title: e.detail.info,
+                artwork: [
+                    // add also 96, 192, 256, 384 and 512
+                    {src: logo128, sizes: '128x128', type: 'image/png'},
+                ],
+            });
+        }
+        chrome.runtime.sendMessage({
+            cmd: MSG_CMD.PLAY,
+            data: {info: e.detail.info, playedMsec: e.detail.playedMsec},
+        });
+    });
+    pl.addEventListener('stop', (e) => {
+        chrome.runtime.sendMessage({
+            cmd: MSG_CMD.STOP,
+            data: {playedMsec: e.detail.playedMsec},
+        });
+    });
+    return pl;
+};
+
 chrome.runtime.onMessage.addListener((message) => {
     if ('play' in message) {
         if (!player) {
-            player = new Player(message.play.playlist, KGM_MIRRORS);
-            player.addEventListener('play', (e) => {
-                if ('mediaSession' in navigator) {
-                    navigator.mediaSession.metadata = new MediaMetadata({
-                        title: e.detail.info,
-                        artwork: [
-                            // add also 96, 192, 256, 384 and 512
-                            {src: logo128, sizes: '128x128', type: 'image/png'},
-                        ],
-                    });
-                }
-                chrome.runtime.sendMessage({
-                    cmd: MSG_CMD.PLAY,
-                    data: {info: e.detail.info, playedMsec: e.detail.playedMsec},
-                });
-            });
-            player.addEventListener('stop', (e) => {
-                chrome.runtime.sendMessage({cmd: MSG_CMD.STOP, data: {playedMsec: e.detail.playedMsec}});
-            });
+            player = initPlayer(message.play.playlist, KGM_MIRRORS);
         }
-
         player.isPlaying ? player.stop() : player.play();
     }
 });
